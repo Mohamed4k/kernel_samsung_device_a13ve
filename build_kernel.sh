8dceb94120f604858d547404bed6dd97bf5873be
Mohamed4k <aaamohammed603@gmail.com>
2025-01-28 23:15:25 +0000

    a13: import scripts

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
+# copy right by zetaxbyte
+# you can rich me on telegram t.me/@zetaxbyte
+# flags of proton clang
 
-# export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export CROSS_COMPILE=$(pwd)/toolchain/toolchains-gcc-10.3.0/bin/aarch64-buildroot-linux-gnu-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
-export CLANG_TRIPLE=aarch64-linux-gnu-
-export ARCH=arm64
-#export ANDROID_MAJOR_VERSION=r
+sudo apt-get install clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang -y
 
-export KCFLAGS=-w
-export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+sudo apt-get install gcc-aarch64-linux-gnu bc -y
 
-make clean
-make mrproper
+git clone https://gitlab.com/LeCmnGend/proton-clang  -b clang-18 --depth=1 $(pwd)/proton-clang
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y sudo_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j32
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
 
-cp out/arch/arm64/boot/Image.gz $(pwd)/arch/arm64/boot/Image.gz
 
-# make anykernel zip
-# cp $(pwd)/arch/arm64/boot/Image $(pwd)/anykernel/
-# zip -0 $(pwd)/output/anykernel.zip $(pwd)/anykernel/*
+# change DEFCONFIG to you are defconfig name or device codename
+
+DEFCONFIG="sudo_defconfig"
+
+# you can set you name or host name(optional)
+
+export KBUILD_BUILD_USER="ColoroXKernel"
+export KBUILD_BUILD_HOST="specific cat"
+
+# do not modify TC_DIR and export PATCH it's been including with the proton-clang dir
+
+TC_DIR="$(pwd)/proton-clang"
+
+export PATH="$TC_DIR/bin:$PATH"
+export CONFIG_NO_ERROR_ON_MISMATCH=y
+export CONFIG_DEBUG_SECTION_MISMATCH=y
+mkdir -p out
+make O=out CC=clang ARCH=arm64 $DEFCONFIG
+
+make O=out -j32 ARCH=arm64 CC=clang AR=llvm-ar NM=llvm-nm OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- 2>&1 | tee log.txt
+
